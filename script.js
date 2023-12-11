def34fefe74a03bcdbca509278352e3b33fee698
Atindra Singh Rathore <118996145+AtindraRathore@users.noreply.github.com>
2023-12-11 17:50:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+let a = Integer.parseInt(prompt("Enter the number: "));
+let b = Integer.parseInt(prompt("Enter the number: "));
+a = a*2;
+a+=b;
+alert("The final result is "+a);
